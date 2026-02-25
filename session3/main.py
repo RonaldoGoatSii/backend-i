8b1680c4f9dc06ec088b8d0b5aa6cf4331cf3eb8
RonaldoGoatSii <172612901+RonaldoGoatSii@users.noreply.github.com>
2026-02-25 21:56:41 +0000

    feat: working on session 3 exercises

--- session3/main.py
+++ session3/main.py
@@ -1,17 +1,15 @@
-from typer import Typer, echo,style,colors
+from typer import Typer, echo, style, colors
 from datetime import datetime
 
 cli = Typer()
 
 @cli.command()
-def greetings():
-    echo(style("Greetig", fg=colors.BLUE))
+def greetings(name: str, surname = ""):
+    echo(style(f"Greetings {name and surname}", fg=colors.BLUE))
 
 @cli.command()
 def goodbye():
     echo(style("Goodbye!", fg=colors.YELLOW))
-
-
 
 if __name__ == "__main__":
     cli()
