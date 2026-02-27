ed0bf7035de1d41dbae770265f8584640b7ca2f9
RonaldoGoatSii <tiagolinsbrasileiro@gmail.com>
2026-02-27 16:28:13 +0000

    feat: trying to debug session3

--- session3/main.py
+++ session3/main.py
@@ -1,15 +1,36 @@
+from dataclasses import dataclass
 from typer import Typer, echo, style, colors
 from datetime import datetime
 
+
+@dataclass
+class Meeting:
+    title :str
+    owner :str
+    date  :str
+
 cli = Typer()
 
-@cli.command()
-def greetings(name: str, surname = ""):
-    echo(style(f"Greetings {name and surname}", fg=colors.BLUE))
 
 @cli.command()
-def goodbye():
-    echo(style("Goodbye!", fg=colors.YELLOW))
+def greetings(name: str = ""):
+    echo(style(f"Greeting {name}", fg=colors.BLUE))
+
+@cli.command()
+def greatings():
+    echo(style("Bye", fg=colors.YELLOW))
+
+@cli.command()
+def create_meating(meeting: Meeting):
+    """
+    Command to create a new meeting note
+    """
+    echo(style(f"""
+    ===========================
+               {meeting.title}
+    ===========================
+    created by {meeting.owner}© on {meeting.date}
+    """, fg=colors.BRIGHT_CYAN, bold=True))
 
 if __name__ == "__main__":
     cli()
