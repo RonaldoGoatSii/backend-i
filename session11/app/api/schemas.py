faeaa09e5d8ef56a5f1eeb5c8dcab3749f10ff58
RonaldoGoatSii <tiagolinsbrasileiro@gmail.com>
2026-04-03 09:08:23 +0000

    chore: made fastApi run correctly

--- session11/app/api/schemas.py
+++ session11/app/api/schemas.py
@@ -2,10 +2,8 @@ from pydantic import BaseModel, Field
 from datetime import date
 
 class ActionItemCreate(BaseModel):
-    # Challenge: O 'min_length=2' garante que o owner não seja vazio
     description: str = Field(..., min_length=3, description="O que tem de ser feito")
     owner: str = Field(..., min_length=2, description="Quem vai fazer")
-    # Challenge: Usar 'date' em vez de 'str' valida automaticamente a data
     due_date: date 
 
 class ActionItemRead(ActionItemCreate):
